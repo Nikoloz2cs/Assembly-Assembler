beb23911400a2860f15d531422594f62a7b203dc
Nikoloz Gvelesiani <nikolozgvelesiani10@gmail.com>
2025-02-23 01:14:06 -0500

    Fix add bug, change all vars to snake_case,spacing

--- Checkpoint_1/project1.cpp
+++ Checkpoint_1/project1.cpp
@@ -59,67 +59,67 @@ int main(int argc, char* argv[]) {
 
     // store data in inst_labels dictionary
 
-    auto dataDir = std::find(instructions.begin(), instructions.end(), ".data");
-    auto textDir = std::find(instructions.begin(), instructions.end(), ".text");
-    auto globlDir = std::find(instructions.begin(), instructions.end(), ".globl main");
+    auto data_dir = std::find(instructions.begin(), instructions.end(), ".data");
+    auto text_dir = std::find(instructions.begin(), instructions.end(), ".text");
+    auto globl_dir = std::find(instructions.begin(), instructions.end(), ".globl main");
 
-    int lineNo = 0; // instruction line number starting at 0 (incremet of 0)
-    auto instIter = globlDir + 1;
-    while (instIter != instructions.end()) {
-        if (isLabel(*instIter) == -1){ // if this instruction is NOT a label
-            lineNo++;
-            // std::cout << lineNo << " " << *instIter << std::endl;
-            instIter++; // increase the index only when NOT a label
+    int line_no = 0; // instruction line number starting at 0 (incremet of 0)
+    auto inst_iter = globl_dir + 1;
+    while (inst_iter != instructions.end()) {
+        if (isLabel(*inst_iter) == -1){ // if this instruction is NOT a label
+            line_no++;
+            // std::cout << line_no << " " << *inst_iter << std::endl;
+            inst_iter++; // increase the index only when NOT a label
         }
         else { // if this instruction is a label
-            inst_labels[split(*instIter, ":")[0]] = lineNo;
-            instructions.erase(instIter);
+            inst_labels[split(*inst_iter, ":")[0]] = line_no;
+            instructions.erase(inst_iter);
         }
     }
 
     // print all elements in the dictionary 
-    std::unordered_map<std::string, int>::iterator instLabelsItr;
+    std::unordered_map<std::string, int>::iterator inst_labels_itr;
     std::cout << "\nAll Elements in inst_labels: \n";
-    for (instLabelsItr = inst_labels.begin(); instLabelsItr != inst_labels.end(); instLabelsItr++) {
-      std::cout << instLabelsItr -> first << " " << instLabelsItr -> second << std::endl;
+    for (inst_labels_itr = inst_labels.begin(); inst_labels_itr != inst_labels.end(); inst_labels_itr++) {
+        std::cout << inst_labels_itr -> first << " " << inst_labels_itr -> second << std::endl;
     }
 
 
     // store data in static_memory vector
-    int staticAdress = 0; // address in static memory in bytes starting at 0 (increment of 4 bytes)
+    int static_address = 0; // address in static memory in bytes starting at 0 (increment of 4 bytes)
     // iterate through static memory lables
-    for (auto staticIter = dataDir + 1; staticIter != textDir; staticIter++) {
-        std::vector<std::string> staticMemTerms = split(*staticIter, " :");
-        // staticMemTerms: { label, directive (.word, .asciiz, etc), data to store, ... }
-        static_memory_labels[staticMemTerms[0]] = staticAdress;
+    for (auto static_iter = data_dir + 1; static_iter != text_dir; static_iter++) {
+        std::vector<std::string> static_mem_terms = split(*static_iter, " :");
+        // static_mem_terms: { label, directive (.word, .asciiz, etc), data to store, ... }
+        static_memory_labels[static_mem_terms[0]] = static_address;
 
-        if (staticMemTerms[1] == ".word") { // if the directive is ".word"
+        if (static_mem_terms[1] == ".word") { // if the directive is ".word"
             // iterate through elements to store
-            for (auto termIter = staticMemTerms.begin() + 2; termIter != staticMemTerms.end(); termIter++){
-                std::string term = *termIter;
+            for (auto term_iter = static_mem_terms.begin() + 2; term_iter != static_mem_terms.end(); term_iter++){
+                std::string term = *term_iter;
     
                 if ((term.length() == 1) and isdigit(term[0])) { // if the term is numerical data, NOT label // term[0] (as opposed to just term) is to convert basic_string<char> to char
-                    static_memory.push_back(std::stoi(*termIter));
+                    static_memory.push_back(std::stoi(*term_iter));
                 }
                 else { // if the term is label
                     static_memory.push_back(4 * inst_labels[term]); // push it in bytes (e.g. Line 7 would be 28)
                 }
-                staticAdress += 4;
+                static_address += 4;
             }
         }
 
-        else if (staticMemTerms[1] == ".asciiz") { // if the directive is ".asciiz" (null-terminating string)
-            std::string staticMemString = split(*staticIter, "\"")[1]; // this gives the first term after ", which is the string to be stored
+        else if (static_mem_terms[1] == ".asciiz") { // if the directive is ".asciiz" (null-terminating string)
+            std::string static_mem_string = split(*static_iter, "\"")[1]; // this gives the first term after ", which is the string to be stored
 
             // iterate through characters in the string
-            for (char c : staticMemString){
+            for (char c : static_mem_string){
                 static_memory.push_back((int) c);
             }
             static_memory.push_back(char('\n'));
         }
     }
     
-    instructions.erase(dataDir, globlDir + 1); // erase through beginning to main
+    instructions.erase(data_dir, globl_dir + 1); // erase through beginning to main
     
     // from here, only pure instructions are left in the vector instructions
     
@@ -139,7 +139,7 @@ int main(int argc, char* argv[]) {
     std::unordered_map<std::string, int>::iterator itr;
     std::cout << "\nAll Elements in static_memory_labels: \n";
     for (itr = static_memory_labels.begin(); itr != static_memory_labels.end(); itr++) {
-      std::cout << itr->first << " " << itr->second << std::endl;
+        std::cout << itr->first << " " << itr->second << std::endl;
     }
 
 
@@ -147,7 +147,6 @@ int main(int argc, char* argv[]) {
     
     /** Phase 2
      * Process all static memory, output to static memory file
-     * TODO: All of this
      */
     // std::cout << "first element in static_mem "  << static_memory[0] << std::endl;
     for (int static_mem_code : static_memory) {
@@ -157,54 +156,50 @@ int main(int argc, char* argv[]) {
 
     /** Phase 3
      * Process all instructions, output to instruction memory file
-     * TODO: Almost all of this, it only works for adds
      */
     for(std::string inst : instructions) {
         std::vector<std::string> terms = split(inst, WHITESPACE+",()");
         std::string inst_type = terms[0];
+
         if (inst_type == "add") {
-            int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 32);
-            write_binary(result, inst_outfile);
-        }
-        if (inst_type == "add") {
-            int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 32);
+            int result = encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 32);
             write_binary(result,inst_outfile);
         }
         //sub encoding
         else if (inst_type == "sub")
         {
-            int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 34);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 34);
+            write_binary(result, inst_outfile);
         }
         //mult
         else if (inst_type == "mult")
         {
-            int result = encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 24);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, registers[terms[1]], registers[terms[2]], 0, 0, 24);
+            write_binary(result, inst_outfile);
         }
         //div
         else if (inst_type == "div")
         {
-            int result = encode_Rtype(0,registers[terms[1]], registers[terms[2]], 0, 0, 26);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, registers[terms[1]], registers[terms[2]], 0, 0, 26);
+            write_binary(result, inst_outfile);
         }
         //mflo
         else if (inst_type == "mflo")
         {
-            int result = encode_Rtype(0,0, 0, registers[terms[1]], 0, 18);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, 0, 0, registers[terms[1]], 0, 18);
+            write_binary(result, inst_outfile);
         }
         //mfhi
         else if (inst_type == "mfhi")
         {
-            int result = encode_Rtype(0,0, 0, registers[terms[1]], 0, 16);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, 0, 0, registers[terms[1]], 0, 16);
+            write_binary(result, inst_outfile);
         }
         //slt
         else if (inst_type == "slt")
         {
-            int result = encode_Rtype(0,registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 42);
-            write_binary(result,inst_outfile);
+            int result = encode_Rtype(0, registers[terms[2]], registers[terms[3]], registers[terms[1]], 0, 42);
+            write_binary(result, inst_outfile);
         }
         //sll
     }
